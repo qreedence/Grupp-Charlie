acbf9f64d14594bd5f3ee7d09636288acf80df31
qreedence <eden.yusof.ioannidis@gmail.com>
2024-04-11 15:00:26 +0200

    Add API Controllers

--- API/Data/Repositories/HouseRepository.cs
+++ API/Data/Repositories/HouseRepository.cs
@@ -1,8 +1,26 @@
 ﻿using API.Data.Interfaces;
+using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data.Repositories
 {
     public class HouseRepository : IHouse
     {
+        private readonly ApplicationDbContext applicationDbContext;
+        public HouseRepository(ApplicationDbContext ApplicationDbContext) 
+        {
+            applicationDbContext = ApplicationDbContext;
+        }
+
+        public async Task AddAsync(House house)
+        {
+            await applicationDbContext.Houses.AddAsync(house);
+            await applicationDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<House>> GetAllAsync()
+        {
+            return await applicationDbContext.Houses.ToListAsync();
+        }
     }
 }
