be3e8f4dfca453b1a9a33a4e1f62166155a29a9f
qreedence <eden.yusof.ioannidis@gmail.com>
2024-04-26 13:32:05 +0200

    Added seed counties functionality

---
+++ API/Controllers/JsonController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data.Interfaces;
+using API.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JsonController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Json", "counties.json");
+                string jsonContent = System.IO.File.ReadAllText(jsonFilePath);
+                return Content(jsonContent, "application/json");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
+
+
+
---
+++ Client/Models/JsonCounty.cs
@@ -0,0 +1,36 @@
+﻿namespace Client.Models
+{
+    public class Geometry
+    {
+        public List<List<List<double>>> coordinates { get; set; }
+        public string type { get; set; }
+    }
+
+    public class GeoPoint2d
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class GeoShape
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Properties
+    {
+    }
+
+    public class JsonCounty
+    {
+        public GeoPoint2d geo_point_2d { get; set; }
+        public GeoShape geo_shape { get; set; }
+        public string year { get; set; }
+        public List<string> lan_code { get; set; }
+        public List<string> lan_name { get; set; }
+        public string lan_area_code { get; set; }
+        public string lan_type { get; set; }
+    }
+}
