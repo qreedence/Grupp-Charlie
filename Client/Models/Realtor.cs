10ba815975aad33f2f0144f1f3e669bf994f4a39
Susanna Renström <s.renstrom@outlook.com>
2024-05-10 15:44:11 +0200

    Added some changes to foreach in updatehouse

--- Client/Models/Realtor.cs
+++ Client/Models/Realtor.cs
@@ -12,7 +12,6 @@
         public string? PhoneNumber { get; set; }
         public bool? EmailConfirmed { get; set; }
 
-        public string? Password { get; set; }
         //public string? PasswordHash { get; set; }
     }
 }
