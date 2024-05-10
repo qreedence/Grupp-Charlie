cbf8c648ab37715149f31677fb3308cd00a9b0d2
Susanna Renström <s.renstrom@outlook.com>
2024-05-10 10:51:05 +0200

    Added some changes to InactiveRealtorAccounts, navbar and login

--- Client/Models/Realtor.cs
+++ Client/Models/Realtor.cs
@@ -10,8 +10,9 @@
         public string? UserName { get; set; }
         public string? Avatar { get; set; }
         public string? PhoneNumber { get; set; }
-
         public bool? EmailConfirmed { get; set; }
+
+        public string? Password { get; set; }
         //public string? PasswordHash { get; set; }
     }
 }
